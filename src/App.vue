ebbef55edf16e1da37976ec1bad6c717a6b1496c
kutluhann <kutluhan22@gmail.com>
2019-11-16 17:37:57 +0300

    Fixes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="countdown">
-    <span>ara tatilin bitmesine ne kadar kaldı?</span>
-    <span class="fields">
-      <span class="field">{{days}}g</span>
-      <span class="field">{{hours}}s</span>
-      <span class="field">{{minustes}}d</span>
-      <span class="field">{{seconds}}s</span>
-    </span>
+  <div class="container" v-if="!loading">
+    <div class="countdown" v-if="!finished">
+      <span>ara tatilin bitmesine ne kadar kaldı?</span>
+      <span class="fields">
+        <span class="field">{{days}}g</span>
+        <span class="field">{{hours}}s</span>
+        <span class="field">{{minustes}}d</span>
+        <span class="field">{{seconds}}s</span>
+      </span>
+    </div>
+    <div v-else>
+      <span>ara tatil bitti, okullara devam :(</span>
+    </div>
   </div>
 </template>
 
@@ -18,29 +23,32 @@ export default {
   },
   data() {
     return {
+      loading: true,
       days: 0,
       hours: 0,
       minustes: 0,
       seconds: 0,
+      finished: false
     }
   },
   computed: {
   },
   mounted() {
+    setTimeout(() => {
+      this.loading = false
+    }, 2000)
     setInterval(() => {
       let finalDate = new Date("Nov 25, 2019 00:00:00").getTime()
       let now = Date.now()
       let distance = finalDate - now
-      this.days = Math.floor(distance / (1000 * 60 * 60 * 24))
-      this.hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
-      this.minustes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60))
-      this.seconds = Math.floor(distance % (1000 * 60) / (1000))
-      /*
-      this.days = days < 10 ? '0' + days : days
-      this.hours = hours < 10 ? '0' + hours : hours
-      this.minustes = minustes < 10 ? '0' + minustes : minustes
-      this.seconds = seconds < 10 ? '0' + seconds : seconds
-      */
+      if(distance > 0){
+        this.days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        this.hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+        this.minustes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60))
+        this.seconds = Math.floor(distance % (1000 * 60) / (1000))
+      }else{
+        this.finished = true
+      }
     }, 1000)
   }
 }
@@ -51,29 +59,44 @@ export default {
   margin: 0;
   padding: 0;
 }
+html{
+  font-size: 16px;
+}
 body{
   height: 100vh;
   width: 100vw;
-  font-size: 1rem;
   font-family: 'Open Sans', sans-serif;
+  text-align: center;
+  overflow: hidden;
+}
+@media (max-width: 1024px){
+  html{
+    font-size: 8px;
+  }
 }
 span{
   font-size: 2rem;
+  font-weight: bold;
 }
-.countdown{
-  height: 100vh;
-  width: 100vw;
+.container{
+  height: 100%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-weight: bold;
+}
+.countdown{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 .fields{
-  margin-top: 4rem;
+  padding: 4rem;
 }
 .field{
-  margin: 1rem;
+  padding: 1rem;
   font-size: 4rem;
 }
 </style>
